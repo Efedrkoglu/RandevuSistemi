abe69b7d7387adcc1a379ae07ee62948fadc5882
Efe Durukoglu <efedurukoglu@gmail.com>
2024-07-26 15:42:16 +0300

    footer margin

--- user/randevuAl.php
+++ user/randevuAl.php
@@ -115,7 +115,7 @@
             <div class="col-3"></div>
         </div>
     </div>
-    <div class="container-fluid mt-2" style="background-color: black;">
+    <div class="container-fluid mt-5" style="background-color: black;">
         <div class="row">
             <div class="col-3"></div>
             <div class="col-6">
